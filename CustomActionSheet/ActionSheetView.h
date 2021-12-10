21480ee83f4a8a8a8d51efe45f5a0aacab5f3821
mapboo <mmw05@163.com>
2021-12-10 12:01:59 +0800

    支持数组传值

---
+++ CustomActionSheet/ActionSheetView.h
@@ -0,0 +1,27 @@
+//
+//  ActionSheetView.h
+//  ActionSheet
+//
+//  Created by Doman on 17/4/17.
+//  Copyright © 2017年 doman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ActionSheetItem.h"
+
+typedef void(^ClickBlock)(ActionSheetItem *sheetItem);
+
+@interface ActionSheetView : UIView
+
+@property (nonatomic,copy)ClickBlock clickBlock;
+
+
+- (instancetype)initWithCancleTitle:(NSString *)cancleTitle
+                     otherTitles:(NSString *)otherTitles,... NS_REQUIRES_NIL_TERMINATION;
+
+- (instancetype)initWithCancleTitle:(NSString *)cancleTitle otherTitlesArr:(NSArray *)otherTitles;
+
+- (void)show;
+
+
+@end
